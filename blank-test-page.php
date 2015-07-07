bf2097c367718ec97fc5ad20a5c45df17cd9d029
testuser272 <testuser272@outlook.com>
2015-07-07 14:16:49 -0500

    Create blank-test-page.php
    This is an empty template for running a quick test.

---
+++ blank-test-page.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+</head>
+
+<body lang="en-US">
+
+
+
+<?php
+
+
+
+?>
+
+</body>
+</html>
